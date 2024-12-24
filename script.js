8b9ebb6a60423239c7b982f72d66718ac9cb14bf
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-12-24 14:55:10 +0500

    finish and optimisation modals

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    
+
+    const modalBtns = document.querySelectorAll('.show-modal-window'),
+        modals = document.querySelectorAll('.modal-window'),
+        closeBtnModal = document.querySelectorAll('.close-modal-window'),
+        overlay = document.querySelector('.overlay'),
+        body = document.querySelector('body')
+    
+    function showModal(modal) {
+        modals.forEach(item => {
+            body.style.height = '100vh'
+            overlay.classList.remove('hidden')
+            item.classList.remove('hidden')
+        })
+    }
+
+    function hideModal() {
+        modals.forEach(item => {
+            item.classList.add('hidden')
+            overlay.classList.add('hidden')
+            body.style.height = ''
+        })
+    }
+
+    modalBtns.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            const targetModal = modals[index]
+            showModal(targetModal)
+        })
+    })
+
+    closeBtnModal.forEach(btn => {
+        btn.addEventListener('click', () => hideModal())
+    })
+
+    overlay.addEventListener('click', (e) => {
+        if (e.target === overlay) {
+            hideModal()
+        }
+       
+    })
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            hideModal()
+        }
+    })
+    
+})
+
+
